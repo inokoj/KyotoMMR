a177a29ea31c5592ec6b8e3ca3ad67719d1005b4
DiveshLala <divesh.lala@gmail.com>
2021-11-11 14:06:20 +0900

    Add output stream for client

--- SensorController/src/main/Client.java
+++ SensorController/src/main/Client.java
@@ -1,7 +1,10 @@
 package main;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client{
@@ -10,12 +13,25 @@ public class Client{
 	BufferedReader in;
 	boolean isConnected;
 	public String name;
+	PrintWriter writer;
+	
 	
 	public Client(Socket s, BufferedReader br, String n) {
 		socket = s;
 		in = br;
 		name = n;
 		isConnected = true;
+		
+		try {
+			writer = new PrintWriter(s.getOutputStream(), true);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(String message) {
+		writer.println(message);
 	}
 
 }
